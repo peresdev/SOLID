076bdd18c2d907ea99c75bc546a1c6050fe19282
Leandro Peres <leandroperesphp@gmail.com>
2020-10-15 15:07:09 -0300

    DIP/ISP

--- SOLID.DIP/Program.cs
+++ SOLID.DIP/Program.cs
@@ -1,12 +1,84 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
 
 namespace SOLID.DIP
 {
-    class Program
+    public enum Relationship
     {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public class Person
+    {
+        public string Name;
+        //public DateTime DateOfBirth { get; set; }
+    }
+
+    #region DIP Solution
+    public interface IRelationShipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+    #endregion
+
+    // low-level
+    public class Relationships : IRelationShipBrowser
+    {
+        public List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
+        #region DIP Solution
+        public IEnumerable<Person> FindAllChildrenOf(string name)
+        {
+           return relations.Where(
+             x => x.Item1.Name == name &&
+                  x.Item2 == Relationship.Parent
+           ).Select(r => r.Item3);
+        }
+        #endregion
+    }
+
+    public class Research
+    {
+        #region DIP Violation
+        public Research(Relationships relationships)
+        {
+            var relations = relationships.relations;
+            foreach (var r in relations.Where(
+               x => x.Item1.Name == "Leandro" &&
+               x.Item2 == Relationship.Parent))
+            {
+                WriteLine($"Leandro has a child called {r.Item3.Name}");
+            }
+        }
+        #endregion
+
+        #region DIP Solution
+        public Research(IRelationShipBrowser browser)
+        {
+            foreach (var p in browser.FindAllChildrenOf("Leandro"))
+                WriteLine($"Leandro has a child called {p.Name}");
+        }
+        #endregion
         static void Main(string[] args)
         {
-            
+            var parent = new Person { Name = "Leandro" };
+            var child1 = new Person { Name = "João" };
+            var child2 = new Person { Name = "Antônio" };
+
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
+
+            new Research(relationships);
         }
     }
 }
--- SOLID.ISP/Program.cs
+++ SOLID.ISP/Program.cs
@@ -2,7 +2,117 @@
 
 namespace SOLID.ISP
 {
-    class Program
+    public class Document
+    {
+
+    }
+
+    #region SRP Violation
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+
+        public void Fax(Document d)
+        {
+            //
+        }
+    }
+
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+
+    #region SRP Solution
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScanner.Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IMultiFunctionDevice : IScanner, IPrinter //...
+    {
+
+    }
+
+    public class MultiFunctionMachine : IMultiFunctionDevice
+    {
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(printer));
+            }
+            if (scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        } // decorator pattern!
+    }
+    #endregion
+
+    public class Program
     {
         static void Main(string[] args)
         {
